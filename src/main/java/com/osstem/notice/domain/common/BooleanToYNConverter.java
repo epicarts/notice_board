fa76940fc0ca6c2b87c1db0e9f141b0995be7c6a
epicarts <0505zxc@gmail.com>
2021-12-03 11:41:26 +0900

    create로 변경 / converter 주석 추가

--- src/main/java/com/osstem/notice/domain/common/BooleanToYNConverter.java
+++ src/main/java/com/osstem/notice/domain/common/BooleanToYNConverter.java
@@ -6,10 +6,12 @@ import javax.persistence.Converter;
 @Converter
 public class BooleanToYNConverter implements AttributeConverter<Boolean, String> {
 
+    // Boolean 을 Y, N 으로 변환
     public String convertToDatabaseColumn(Boolean attribute) {
         return (attribute != null && attribute) ? "Y" : "N";
     }
 
+    // Y, N 을 Boolean 으로 변환
     public Boolean convertToEntityAttribute(String s) {
         return "Y".equals(s);
     }
