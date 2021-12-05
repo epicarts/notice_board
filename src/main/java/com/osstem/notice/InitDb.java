7fa0250e753bff588503a3a7c2b6ec0270b9a5aa
YoungHo Choi <0505zxc@gmail.com>
2021-12-05 21:15:06 +0900

    초기 데이터 자동 입력 구현

---
+++ src/main/java/com/osstem/notice/InitDb.java
@@ -0,0 +1,72 @@
+package com.osstem.notice;
+
+import com.osstem.notice.domain.board.Comment;
+import com.osstem.notice.domain.board.Notice;
+import com.osstem.notice.domain.user.Role;
+import com.osstem.notice.domain.user.User;
+import com.osstem.notice.repository.CommentRepository;
+import com.osstem.notice.repository.NoticeRepository;
+import com.osstem.notice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.PostConstruct;
+
+/**
+ *
+ */
+@Component
+@RequiredArgsConstructor
+public class InitDb {
+    private final InitService initService;
+
+    @PostConstruct
+    public void init() {
+        initService.dbInit1();
+        initService.dbInit2();
+    }
+
+    @Component
+    @Transactional
+    @RequiredArgsConstructor
+    static class InitService {
+        private final NoticeRepository noticeRepository;
+        private final CommentRepository commentRepository;
+        private final UserRepository userRepository;
+
+        public void dbInit1() {
+            User user = User.createUser("최영호", "0505zxc@osstem.com", "OW공통개발실", Role.ADMIN);
+            userRepository.save(user);
+
+            Notice notice = Notice.createNotice("첫번째 공지사항 제목입니다", "첫번째 공자사항 내용입니다", true);
+            noticeRepository.save(notice);
+
+            Comment comment1 = Comment.createComment(notice, "댓글입니다", 0L);
+            commentRepository.save(comment1);
+
+            Comment comment2 = Comment.createComment(notice, "대댓글입니다1", 1L);
+            commentRepository.save(comment2);
+
+            Comment comment3 = Comment.createComment(notice, "대댓글입니다2", 1L);
+            commentRepository.save(comment3);
+        }
+
+        public void dbInit2() {
+            User user = User.createUser("홍길동", "hong@osstem.com", "법무부", Role.USER);
+            userRepository.save(user);
+
+            Notice notice = Notice.createNotice("두번째 공지사항 제목입니다", "두번째 공자사항 내용입니다", true);
+            noticeRepository.save(notice);
+
+            Comment comment1 = Comment.createComment(notice, "홍길동 댓글입니다.", 0L);
+            commentRepository.save(comment1);
+
+            Comment comment2 = Comment.createComment(notice, "홍길동 대댓글입니다1", 1L);
+            commentRepository.save(comment2);
+
+            Comment comment3 = Comment.createComment(notice, "홍길동 대댓글입니다2", 1L);
+            commentRepository.save(comment3);
+        }
+    }
+}
