9c00ce8ecc50c90995c5d45657dc3c0b385d561e
epicarts <0505zxc@gmail.com>
2021-12-17 16:59:43 +0900

    댓글 사이즈 검증하는 로직 추가

--- src/main/java/com/osstem/notice/controller/vo/CreateCommentRequestVo.java
+++ src/main/java/com/osstem/notice/controller/vo/CreateCommentRequestVo.java
@@ -9,12 +9,14 @@ import org.hibernate.validator.constraints.Range;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Getter
 @RequiredArgsConstructor
 public class CreateCommentRequestVo {
     @NotNull
     @NotBlank(message = "댓글 내용은 필수 입니다") // Bad Request
+    @Size(min = 1, max = 255)
     private String content = "";
 
     @Range(min = 0L)
